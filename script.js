b46f49d4aede19c72a48ba0cd35ddf90b915c3bd
Martina <a.martina.eriksson@hotmail.com>
2020-11-24 15:23:49 +0100

    Ta bort/visa lista

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let activities = [];
 
-$("#form").submit(function(event)){
+function populateList(){
+    let activity = {
+        activity: "Hämta test"
+    }
+
+    let activity2 = {
+        activity: "Justera glasögon"
+    }
+
+    activities.push(activity);
+    activities.push(activity2);
+}
+
+$("#form").submit(function(event){
     event.preventDefault();
     let userInput = $("#user-input").val();
 
@@ -8,5 +21,32 @@ $("#form").submit(function(event)){
 
     activities.push(eventToAdd);
     $("#user-input").val("");
-    
+    displayList();
+})
+
+function displayList() {
+    let list = $("#ul-list");
+    //list.empty();               // LÄGG TILL FUNKTION
+
+    for (let i = 0; i< activities.length; i++){
+        list.append (`<li> ${activities[i].activity} <span onmouseover="style ='cursor: pointer'">X</span></li><br>`)
+    }
+
+    deleteItem();               
+    //updateItem();               //LÄGG TILL FUNKTION
+    //onItemHover();              //LÄGG TILL FUNKTION
 }
+
+function deleteItem() {
+    let allItems = $("span");
+
+    for (let i = 0; i < allItems.length; i++) {
+        $(allItems[i]).click( function () {
+            let parentElement = this.parentElement;
+            parentElement.style.display = "none";
+            activities.splice(i, 1);
+        })
+    }
+}
+
+displayList();
